54c933959d253ffb09432d026c7b26d869a696be
baloh <balohbaloh@yahoo.co.id>
2021-03-21 18:48:18 +0700

    Add typeBasic

---
+++ build/typeBasic.js
@@ -0,0 +1,18 @@
+"use strict";
+// string
+var nama = "Nicktra";
+nama = "string";
+// number
+var umur;
+umur = 30;
+umur = 13;
+// boolean
+var isMarried;
+isMarried = true;
+isMarried = false;
+// any
+var heroes = "Iron Man";
+heroes = 20;
+heroes = [];
+heroes = {};
+heroes = true || false || "string";
---
+++ src/typeBasic.ts
@@ -0,0 +1,20 @@
+// string
+let nama: string = "Nicktra";
+nama = "string";
+
+// number
+let umur: number;
+umur = 30;
+umur = 13;
+
+// boolean
+let isMarried: boolean;
+isMarried = true;
+isMarried = false;
+
+// any
+let heroes: any = "Iron Man";
+heroes = 20;
+heroes = [];
+heroes = {};
+heroes = true || false || "string";
